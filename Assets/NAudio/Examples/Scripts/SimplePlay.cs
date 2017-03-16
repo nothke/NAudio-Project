bb7585bd4c8a04ac2c87394e0b1316f9a144ebbb
Ivan Notaros <nothke@gmail.com>
2017-03-16 08:10:44 +0100

    Added a simple example with play on mouse click

---
+++ Assets/NAudio/Examples/Scripts/SimplePlay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlay : MonoBehaviour
+{
+
+    public AudioClip[] clips;
+    public float minPitch = 0.9f;
+    public float maxPitch = 1.1f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            clips.Play(Vector3.zero, pitch: Random.Range(minPitch, maxPitch));
+    }
+}
