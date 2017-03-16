8cf974afc49533b628202ae01a45b4fb920cb711
Ivan Notaros <nothke@gmail.com>
2017-03-16 08:55:54 +0100

    Added simple engine example

---
+++ Assets/NAudio/Examples/Scripts/EngineAudio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineAudio : MonoBehaviour
+{
+    public AudioClip clip;
+
+    public AnimationCurve volumeCurve;
+    public AnimationCurve pitchCurve;
+
+    float value;
+
+    AudioSource source;
+
+    void Start()
+    {
+        source = NAudio.CreateSource(transform, clip);
+        source.Play();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+            value += Time.deltaTime;
+        else
+            value -= Time.deltaTime;
+
+        value = Mathf.Clamp01(value);
+
+        source.volume = volumeCurve.Evaluate(value);
+        source.pitch = pitchCurve.Evaluate(value);
+    }
+}
