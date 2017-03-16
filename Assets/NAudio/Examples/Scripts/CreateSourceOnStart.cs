1191b91213fc86cbd06b701b2324cb48963f33c0
Ivan Notaros <nothke@gmail.com>
2017-03-16 08:33:27 +0100

    Added creating source on start example

---
+++ Assets/NAudio/Examples/Scripts/CreateSourceOnStart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSourceOnStart : MonoBehaviour {
+    
+    AudioClip clip;
+
+	void Start () {
+        NAudio.CreateSource(transform, clip);
+	}
+}
