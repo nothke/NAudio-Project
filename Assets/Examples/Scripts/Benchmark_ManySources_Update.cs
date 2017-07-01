d5cbe8afb24541efc3c49240019d17502a3d4107
Ivan Notaros <nothke@gmail.com>
2017-07-01 06:03:35 +0200

    Update

--- Assets/Examples/Scripts/Benchmark_ManySources_Update.cs
+++ Assets/Examples/Scripts/Benchmark_ManySources_Update.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Benchmark_ManySources_Update : MonoBehaviour {
-	
-	public AudioClip[] clips;
-	public int ratePerFrame = 1;
-	public float range = 10;
+public class Benchmark_ManySources_Update : MonoBehaviour
+{
 
-	void Update () {
-		for (int i = 0; i < ratePerFrame; i++)
-		{
-			clips.Play(Random.insideUnitSphere * range);
-		}
-	}
+    public AudioClip[] clips;
+    public int ratePerFrame = 1;
+    public float range = 10;
+
+    public bool usePerf;
+
+    void Update()
+    {
+        for (int i = 0; i < ratePerFrame; i++)
+        {
+            if (!usePerf)
+                clips.Play(Random.insideUnitSphere * range);
+        }
+    }
 }
